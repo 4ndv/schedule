a752b644494e650d3c67458bb3051a2c94e229a3
Andrey Viktorov <andreyviktorov@outlook.com>
2016-12-16 15:38:51 +0700

    Institutes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'institutes#index'
+
+  resources :institutes
+  resources :groups
+  resources :items
+  resources :lessons
+  resources :teachers
 end
